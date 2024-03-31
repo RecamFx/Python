5ffccee242f35ac1de7df7b502ab19e6660c5610
RecamFx <camiloreartesgm@gmail.com>
2024-03-31 18:28:15 -0300

    Automatic Terminal

--- Parte 2/funciones/parametro_args.py
+++ Parte 2/funciones/parametro_args.py
@@ -25,3 +25,31 @@ def suma3(name, *num): # El asterisco(*) junta a todos los parametros como si fu
     
 suma3("Stephen",1,2,3,4,5,6,7) #? 28
 # Ya no necesitamos crear una lista
+
+
+
+#--------------------------------------------------------------------------------------------------#
+
+
+
+# PARAMETROS NO POSICIONALES
+def asd(nombre, apellido):
+    print(f"Hola {nombre} {apellido}")
+    
+# Definimos los parametros con igual
+asd(apellido="Curry", nombre="Stephen") # Son parametros forzados #? Hola Stephen Curry
+
+
+
+#--------------------------------------------------------------------------------------------------#
+
+
+
+# Cambiando un parametro opcional
+# El parametro apellido ya viene por defecto con un valor, es opcional cambiarlo
+def prueba(nombre, apellido = "pruebadeapellido"):
+    print(f"Hola {nombre} {apellido}")
+    
+prueba("Stephen")  #? Hola Stephen pruebadeapellido
+prueba("Stephen", "Curry")  #? Hola Stephen Curry
+prueba("Stephen", apellido="Curry")  #? Hola Stephen Curry
