ff4acd0f2d80d5765a18fc3b8a341f2b8cc0a14e
RecamFx <camiloreartesgm@gmail.com>
2024-03-31 18:00:51 -0300

    Automatic Terminal

--- Parte 2/funciones/crear_funciones.py
+++ Parte 2/funciones/crear_funciones.py
@@ -11,6 +11,7 @@ saludar() #? Hola buenos dias
 
 #---------------------------------------------------------------------------------------------------------#
 
+
 # Un parametro es una variable la cual solo existe dentro de la funcion, se crea unicamente para la funcion
 # Funcion con parametros
 
@@ -21,3 +22,39 @@ def saludo(nombre, genero):
         print(f"Hola {nombre} maestra! Como estas?")
     
 saludo("Stephen", "hombre") #? Hola Stephen maestro! Como estas?
+
+
+#---------------------------------------------------------------------------------------------------------#
+
+
+# Crear una funcion que retorne valores
+
+def cuentas(num):
+    jijijija = num * 2
+    return jijijija # Returnea el valor
+
+# Cuando un valor es returneado automaticamente la funcion en este caso "cuenta(2)" se transforma en un valor
+# Por ende el valor si lo ponemos en la nada va a desaparecer
+# Entonces lo imprimimos
+
+print(cuentas(2)) #? 4
+
+
+
+# Retornar multiples valores
+
+def jiji(num):
+    multiplicacion = num * 2
+    suma = num + 2
+    resta = num - 2
+    
+    return [multiplicacion,suma,resta]
+
+# Nos returnea dependo lo que hayamos hecho anteriormente, como lo puse en corchetes, nos returnea una lista
+# Por defecto si no le ponemos nada, nos returnea una tupla
+print(type(jiji(10))) #? List
+print(jiji(10)) #? [20, 12, 8]
+
+# Desempaquetando:
+mutli,sumita,restita = jiji(10)
+print(mutli)
