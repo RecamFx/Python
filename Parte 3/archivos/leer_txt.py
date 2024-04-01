18ceeecf46efdbf12a756b8f935af054fdaa3a78
RecamFx <camiloreartesgm@gmail.com>
2024-04-01 02:50:59 -0300

    Automatic Terminal

---
+++ Parte 3/archivos/leer_txt.py
@@ -0,0 +1,6 @@
+# OPEN
+# Open es como un import pero para archivos que no son python
+
+archivo = open("c:\\Users\\Camilo\\Documents\\Programacion\\Cursos\\Python\\Parte 3\\archivos\\notas.txt")
+
+print(archivo)
--- Parte 3/paquetes/paquete.py
+++ Parte 3/paquetes/paquete.py
@@ -1,3 +1,5 @@
 import pack1 
+# Importando un packete de modulos
+# Dentro de la carpeta de packetes tenemos que crear un archivo llamado "__init__.py" para decirle a python que es un packete de modulos
 
 print(pack1.__path__) #? Nos dice la ruta del paquete 
