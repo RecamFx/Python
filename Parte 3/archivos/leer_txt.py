69f2164a07c0ead16fd6be08c76517a3f848e211
RecamFx <camiloreartesgm@gmail.com>
2024-04-01 19:48:06 -0300

    Automatic Terminal

--- Parte 3/archivos/leer_txt.py
+++ Parte 3/archivos/leer_txt.py
@@ -1,6 +1,29 @@
 # OPEN
 # Open es como un import pero para archivos que no son python
 
-archivo = open("c:\\Users\\Camilo\\Documents\\Programacion\\Cursos\\Python\\Parte 3\\archivos\\notas.txt")
+# El encoding="UTF-8 sirve para que funcionen las tildes
+archivo = open("Parte 3\\archivos\\notas.txt", encoding="UTF-8")
 
-print(archivo)
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# Dato: Los archivos solo pueden leerse 1 vez
+# Es decir, no podemos poner .read() y despues .readlines()
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# Usamos el metodo read() para leer un txt
+#todo print(archivo.read())
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# Para leer lineas del archivo txt usamos readlines()
+# Los \n son separaciones entre lineas
+#todo print(archivo.readlines())
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# Podemos usar readline()
+# Sirve para leer la primera linea o una cantidad de caracteres asignados
+print(archivo.readline(10)) # Leemos diez caracteres
+
+# -----------------------------------------------------------------------------------------------------------------------#
+# Cerrar el archivo
+archivo.close()
+# Al usar close se cierra el archivo, si lo queremos leer otra vez, tenemos que poner un open()
---
+++ Parte 3/archivos/leer_txt_optimamente.py
@@ -0,0 +1,14 @@
+# Con with hacemos una mini funcion la cual:
+# Abre el txt, ejecuta el codigo, y se cierra el txt al terminar
+# Al cerrarse el txt es mejor, ya que no consume tanto a la PC
+
+with open("Parte 3\\archivos\\notas.txt"):
+    print("Hola")
+    
+# -------------------------------------------------------------------------------------------------------------#
+# Para asignarle un nombre usamos el as nombre
+# Y para asignarle de que queremos que sea, ya sea de lectura, escritura,etc. Ponemos coma despues del archivo y entre comillas R (read), W(write)
+# Por defecto viene de lectura (r)
+
+with open("Parte 3\\archivos\\notas.txt", "r") as archivo:
+    print("Hola")
