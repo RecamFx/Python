a42270f6623fed2e7c806a2966cc29f19c423d70
RecamFx <camiloreartesgm@gmail.com>
2024-04-02 00:32:06 -0300

    Automatic Terminal

---
+++ Parte 3/Resolviendo Problemas/problemas_txt.py
@@ -0,0 +1,14 @@
+# Dos listas
+# Una con nombres y otra con apellidos
+
+nombres = ["Stephen", "Michael", "Manu"]
+apellidos = ["Curry", "Jordan", "Ginobili"]
+
+# DOS FORMAS DE RESOLVERLO
+
+# with open("Parte 3\\Resolviendo Problemas\\archivo.txt", "w",encoding="UTF-8") as txt:
+#     for i in range(len(nombres)):
+#         txt.write(f"{nombres[i]} {apellidos[i]} \n")
+
+with open("Parte 3\\Resolviendo Problemas\\archivo.txt", "w",encoding="UTF-8") as txt:
+    [txt.write(f"{n} {a} \n") for n,a in zip(nombres,apellidos)] # El zip lo utilizamos porque no se puede poner nombre,appellido
